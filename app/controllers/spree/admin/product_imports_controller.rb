967f23275bd7222ce458e65922194f5689ca5546
Pikender Sharma <avid.piks@gmail.com>
2016-10-18 19:15:44 +0530

    Clear Product Related Data and Import with preselected data added

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -1,6 +1,67 @@
 class Spree::Admin::ProductImportsController < Spree::Admin::BaseController
 
   def index
-    render :text => "Works !!"
+    render
+  end
+
+  def reset
+    truncated_list = ['spree_products_taxons', 'spree_option_values_variants', 'spree_products_promotion_rules']
+    result_log = []
+    truncated_list.inject(result_log) do |r,t|
+      c = "TRUNCATE #{t}"
+      ActiveRecord::Base.connection.execute(c)
+      r.push("Command executed #{c}")
+    end
+
+    model_list = %w{ Image OptionType OptionValue Product Property ProductProperty ProductOptionType
+     Variant StockMovement StockItem StockTransfer Taxonomy Taxon ShippingCategory TaxCategory
+    }
+    model_list.inject(result_log) do |r,m|
+      klass = DataShift::SpreeEcom.get_spree_class(m)
+      s = "Clearing model #{klass}"
+      if(klass)
+        begin
+          klass.delete_all
+        rescue => e
+          s += "generated error #{e}"
+        end
+      else
+        s = "#{m} not found or respective Model"
+      end
+      r.push(s)
+    end
+
+    delete_other_than_defaults = %w{ StockLocation }
+    delete_other_than_defaults.inject(result_log) do |r,m|
+      klass = DataShift::SpreeEcom.get_spree_class(m)
+      s = "Clearing Other than Defaults #{klass}"
+      if(klass)
+        begin
+          klass.where.not(default: true).delete_all
+        rescue => e
+          s += "generated error #{e}"
+        end
+      else
+        s = "#{m} not found or respective Model"
+      end
+      r.push(s)
+    end
+
+    redirect_to admin_product_imports_path, flash: {notice: result_log.join("---,---")}
+  end
+
+  def sample_import
+    @imported = false
+    sample_file = Rails.root.join("sample_csv", "SpreeMultiVariant.csv")
+    if(File.exists? sample_file)
+      @csv_table = CSV.open(sample_file, :headers => true).read
+      render
+    else
+      redirect_to admin_product_imports_path, flash: { error: "Sample Missing" }
+    end
+  end
+
+  def import
+    @imported = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resources :product_imports, only: [:index]
+    resources :product_imports, only: [:index] do
+      collection do
+        get :reset
+        get :sample_import
+      end
+    end
   end
 end
