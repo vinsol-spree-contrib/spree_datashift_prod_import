f8332eac6e1677c55983e3d88fb47ceb36e4a9f5
Pikender Sharma <pikender@vinsol.com>
2016-11-02 19:29:28 +0530

    Added Shopify User Import Demo

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,14 @@ Spree::Core::Engine.routes.draw do
         get :download_sample_csv
       end
     end
-
+    resources :user_imports, only: [:index] do
+      collection do
+        get :reset
+        get :sample_import
+        post :sample_csv_import
+        post :user_csv_import
+        get :download_sample_csv
+      end
+    end
   end
 end
